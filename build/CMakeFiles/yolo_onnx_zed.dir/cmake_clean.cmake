215f4e1a90f1b7c6f8533e6dffff674aa8d0a585
cgomolak <cgomolak181@gmail.com>
2024-03-26 20:49:53 -0400

    Added ONNX ZED custom object detection

---
+++ build/CMakeFiles/yolo_onnx_zed.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolo_onnx_zed.dir/src/GLViewer.cpp.o"
+  "CMakeFiles/yolo_onnx_zed.dir/src/GLViewer.cpp.o.d"
+  "CMakeFiles/yolo_onnx_zed.dir/src/main.cpp.o"
+  "CMakeFiles/yolo_onnx_zed.dir/src/main.cpp.o.d"
+  "CMakeFiles/yolo_onnx_zed.dir/src/yolo.cpp.o"
+  "CMakeFiles/yolo_onnx_zed.dir/src/yolo.cpp.o.d"
+  "yolo_onnx_zed"
+  "yolo_onnx_zed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolo_onnx_zed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
